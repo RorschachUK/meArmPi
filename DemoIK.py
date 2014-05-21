625c6b8b5c83909da58c3cfe1723f3b5577abd22
RorschachUK <bob@bobstone.info>
2014-05-21 22:39:08 +0100

    Update DemoIK.py

--- DemoIK.py
+++ DemoIK.py
@@ -20,8 +20,8 @@ def main():
 		arm.gotoPoint(0, 150, 0)
 		arm.gotoPoint(0, 150, 150)
 		arm.gotoPoint(0, 150, 50)
-		arm.gotoPoint(-100, 150, 50)
-		arm.gotoPoint(100, 150, 50)
+		arm.gotoPoint(-150, 150, 50)
+		arm.gotoPoint(150, 150, 50)
 		arm.gotoPoint(0, 150, 50)
 		arm.gotoPoint(0, 100, 50)
 		
