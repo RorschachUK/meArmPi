860feb7591d9011499cdda50416c8a932ac067e8
RorschachUK <bob@bobstone.info>
2014-05-22 09:10:15 +0100

    Update DemoIK.py

--- DemoIK.py
+++ DemoIK.py
@@ -7,27 +7,25 @@
 import meArm
 
 def main():
-	arm = meArm.meArm()
-	arm.begin()
+    arm = meArm.meArm()
+    arm.begin()
 	
-	while True:
-		arm.openGripper()
-		arm.closeGripper()
-		arm.openGripper()
-		arm.closeGripper()
-		arm.openGripper()
-	
-		arm.gotoPoint(0, 150, 50)
-		arm.gotoPoint(0, 150, 0)
-		arm.gotoPoint(0, 150, 150)
-		arm.gotoPoint(0, 150, 50)
-		arm.gotoPoint(-150, 150, 50)
-		arm.gotoPoint(150, 150, 50)
-		arm.gotoPoint(0, 150, 50)
-		arm.gotoPoint(0, 100, 50)
-		
+    while True:
+        arm.openGripper()
+        arm.closeGripper()
+        arm.openGripper()
+        arm.closeGripper()
+        arm.openGripper()
+
+        arm.gotoPoint(0, 150, 50)
+        arm.gotoPoint(0, 150, 0)
+        arm.gotoPoint(0, 150, 150)
+        arm.gotoPoint(0, 150, 50)
+        arm.gotoPoint(-150, 150, 50)
+        arm.gotoPoint(150, 150, 50)
+        arm.gotoPoint(0, 150, 50)
+        arm.gotoPoint(0, 100, 50)
 	return 0
 
 if __name__ == '__main__':
-	main()
-
+    main()
