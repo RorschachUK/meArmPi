4b829d4157d097b235d63df35e155eb3fc47c3b9
RorschachUK <bob@bobstone.info>
2014-11-22 21:36:06 +0000

    Revision detection change contributed by robin28 for #1

--- Adafruit_I2C.py
+++ Adafruit_I2C.py
@@ -17,7 +17,7 @@ class Adafruit_I2C :
       with open('/proc/cpuinfo','r') as f:
         for line in f:
           if line.startswith('Revision'):
-            return 1 if line.rstrip()[-1] in ['1','2'] else 2
+            return 1 if int(line.rstrip()[-4:]) < 3 else 2
     except:
       return 0
 
